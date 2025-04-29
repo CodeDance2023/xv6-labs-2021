fd7f19b5058be38f747fe1c58ae024ea74fc1a4b
CodeDance2023 <lqw286089098@qq.com>
2025-04-29 21:13:40 +0800

    fork测试

---
+++ user/fork.c
@@ -0,0 +1,21 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include <stddef.h>
+
+//fork测试
+int main() {
+    int pid = fork();
+    if (pid == 0) {
+        printf("child: %d\n", pid );
+        exit(0);
+    }
+    else {
+       int status = -11111;
+       int *p = &status;
+        wait(p);
+        printf("status: %d\n", *p);
+        printf("parent: %d\n",pid);
+        exit(0);
+    }
+}
